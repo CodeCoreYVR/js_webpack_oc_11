8b85a8b5787b1b0d2b310db8abb6f0fd872e2440
Hano <hinsul3113@gmail.com>
2019-10-11 14:15:50 -0700

    Configure entry and output

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,28 @@
+const path = require("path");
+
+// entry points
+// https://webpack.js.org/concepts/entry-points/
+
+// output
+// https://webpack.js.org/concepts/output/
 module.exports = {
   mode: "development",
-  devtool: "none"
+  // by default the mode is production but, we can change it
+  // to development here
+  devtool: "none",
+
+  entry: {
+    //   Create a bundle "./dist/client.js" (named after the key)
+    client: "./src/client.js",
+    // Create a bundle "./dist/defaults.js" (named after the key)
+    defaults: "./src/defaults.js",
+    server: "./src/defaults.js"
+  },
+  output: {
+    //   The 'path' property is used to specify the location where
+    // our bundled files are created. By default, it is
+    // path.join(__dirname, 'build');
+    path: path.join(__dirname, "build"),
+    filename: "[name].bundle.js"
+  }
 };
